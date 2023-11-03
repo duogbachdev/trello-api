e871c5fbb145e1cbc18eb7a06b21bd6399c5b419
duogbachdev <contact.duogbachdev@gmail.com>
2023-11-03 10:59:52 +0700

    Validation With Joi

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { boardRoutes } from '~/routes/v1/boardRoutes'
+import { boardRoute } from '~/routes/v1/boardRoute'
 
 const Router = express.Router()
 
@@ -8,6 +8,6 @@ Router.get('/status', (req, res) => {
   res.status(StatusCodes.OK).json({ message: 'API v1 are ready to use.' })
 })
 
-Router.use('/boards', boardRoutes)
+Router.use('/boards', boardRoute)
 
 export const APIs_V1 = Router
--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ import { APIs_V1 } from '~/routes/v1'
 const START_SERVER = () => {
   const app = express()
 
+  // enable req.body json data
+  app.use(express.json())
+
+  // sử dụng apisV1
   app.use('/v1', APIs_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, async () => {
